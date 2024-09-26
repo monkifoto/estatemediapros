412a93a5724f3b69654f2f922488d68bb433943a
Alex Bucse <seattlerealestatephoto@gmail.com>
2024-09-25 21:40:10 -0700

    added functions

--- src/app/Sections/contact-section/contact-section.component.ts
+++ src/app/Sections/contact-section/contact-section.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-section.component.css']
 })
 export class ContactSectionComponent {
+  formData = {
+    name: '',
+    email: '',
+    message: ''
+  };
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.post('https://your-project-id.cloudfunctions.net/sendContactEmail', this.formData)
+      .subscribe(
+        response => console.log('Email sent successfully', response),
+        error => console.error('Error sending email', error)
+      );
+  }
 }
